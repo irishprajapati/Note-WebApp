ff7acedb5697370166211938b36f26890192d78a
Erish <erish968@gmail.com>
2026-07-09 21:18:05 +0545

    feat: update the controller according to service layer

--- src/main/java/org/eris/controller/UserController.java
+++ src/main/java/org/eris/controller/UserController.java
@@ -5,7 +5,11 @@ import org.eris.repository.UserRepository;
 import org.eris.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -25,5 +29,34 @@ public class UserController {
         }catch (Exception e){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+    }
+    @GetMapping("/user/{myId}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id){
+        Optional<User> user = userService.findUserById(id);
+        if(user.isPresent()){
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
+        User userInDb = userService.findByUserName(userName);
+        if(userInDb != null){
+            userInDb.setUserName(user.getUserName());
+            if(user.getPassword() != null && user.getPassword().isBlank()){
+                userInDb.setPassword(user.getPassword());
+            }
+            userService.saveUser(userInDb);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @DeleteMapping
+    public boolean deleteUserByIdAndUserName(@PathVariable Long id){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        userRepository.deleteByUserName(authentication.getName());
+        userRepository.deleteById(id);
+        return true;
     }
 }
